daeaf424d40e4b6e1b83dae6c4126c437fde13e7
Malcolm Perry <malcolm@ebi.ac.uk>
2023-04-19 21:49:03 +0100

    docstrings

--- plyrs/extra.py
+++ plyrs/extra.py
@@ -3,11 +3,18 @@ from .decorators import wrap_polars
 
 
 def if_else(when, then, otherwise):
+    """
+    Functional wrapper of pl.when.then.otherwise
+    """
     return pl.when(when).then(then).otherwise(otherwise)
 
 
 @wrap_polars
 def index(df, name="id", start=1, prefix: str=None):
+    """
+    Adds an index column to the dataframe. Wraps `with_row_columns` but starts
+    from 1, default to "id" and optionally adds a prefix.
+    """
     df = df.with_row_count(name).with_columns(pl.col(name) + start)
     if prefix:
         df = df.with_columns((prefix + pl.col("id").cast(str)).keep_name())
@@ -16,4 +23,7 @@ def index(df, name="id", start=1, prefix: str=None):
 
 @wrap_polars
 def reorder(df, cols):
+    """
+    Reorder dataframe columns.
+    """
     return df.select(cols, pl.all().exclude(cols))
