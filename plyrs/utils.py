b910526aace335c44cce46044c3656d52aa74bf2
Malcolm Perry <malcolm@ebi.ac.uk>
2023-04-20 21:16:19 +0100

    function to inspect col name

--- plyrs/utils.py
+++ plyrs/utils.py
@@ -1,7 +1,7 @@
 import polars as pl
 from polars.dataframe.groupby import GroupBy
 from polars.lazyframe.groupby import LazyGroupBy
-
+import re
 
 _mask = {
     "filter": "where",
@@ -9,6 +9,16 @@ _mask = {
 
 def as_col(x):
     return pl.col(x) if isinstance(x, str) else x
+
+COLNAME = r'^col\("([^"]*)"\)$'
+
+def col_name(x):
+    if isinstance(x, str):
+        return x
+
+    m = re.match(COLNAME, str(x))
+    if m:
+        return m.group(1)
 
 
 def safe_collect(x):
